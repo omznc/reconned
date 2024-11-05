fbc6f253f6977172b340209c3ab6a2fd805534ba
spojskic <pojskicsafet@gmail.com>
2024-11-05 09:38:18 +0100

    add google one tap

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,14 @@
-import { passkeyClient } from "better-auth/client/plugins";
+import { oneTapClient, passkeyClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
 	baseURL: process.env.BETTER_AUTH_URL,
-	plugins: [passkeyClient()],
+	plugins: [
+		passkeyClient(),
+		oneTapClient({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+		}),
+	],
 });
 
 export function useIsAuthenticated() {
