bd819a4bb611554c56d071dc8075131743c419cb
Omar Zunic <contact@omarzunic.com>
2024-11-07 12:26:52 +0100

    Emails

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,4 +1,5 @@
 import { env } from "@/lib/env";
 import { Resend } from "resend";
 
+export const DEFAULT_FROM = "Airsoft BiH <noreply@safetpojskic.com>";
 export const resend = new Resend(env.RESEND_API_KEY);
