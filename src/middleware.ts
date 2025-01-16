ac2bd9723ad757443591c591827dd355884c346a
Omar Zunic <contact@omarzunic.com>
2025-01-16 18:08:43 +0100

    fix: attempt at fixing null session #2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,26 @@
-import { auth } from "@/lib/auth";
-import { authClient } from "@/lib/auth-client";
 import { env } from "@/lib/env";
 import { headers } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export default async function authMiddleware(request: NextRequest) {
-	const session = await authClient.getSession(undefined, {
-		headers: await headers(),
-	});
+	const response = await fetch(
+		process.env.NODE_ENV === "development"
+			? `${request.nextUrl.origin}/api/auth/get-session`
+			: `${env.NEXT_PUBLIC_BETTER_AUTH_URL}/api/auth/get-session`,
+		{
+			headers: await headers(),
+		},
+	);
+	const data = await response.json();
+	console.log(data);
 
-	console.log("session", session);
-
-	if (!session) {
+	if (!data?.session) {
 		return NextResponse.redirect(new URL("/login", request.url));
 	}
 	return NextResponse.next();
 }
 
 export const config = {
-	matcher: ["/dashboard/:path*"],
+	matcher: ["/dashboard/:path"],
 };
