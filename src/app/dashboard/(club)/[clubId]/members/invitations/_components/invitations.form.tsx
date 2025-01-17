8d1a9712d5aa4b10743ba83a2e67d27c31c1b057
Omar Zunic <contact@omarzunic.com>
2025-01-17 20:41:16 +0100

    feat: moving mail sending service
    Signed-off-by: Omar Zunic <contact@omarzunic.com>

--- src/app/dashboard/(club)/[clubId]/members/invitations/_components/invitations.form.tsx
+++ src/app/dashboard/(club)/[clubId]/members/invitations/_components/invitations.form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { sendInvitation } from "@/app/dashboard/(club)/[clubId]/members/invitations/_components/invitations.actions";
+import { sendInvitation } from "@/app/dashboard/(club)/[clubId]/members/invitations/_components/invitations.actions.tsx";
 import { sendInvitationSchema } from "@/app/dashboard/(club)/[clubId]/members/invitations/_components/invitations.schema";
 import { cn } from "@/lib/utils";
 import { Button } from "@components/ui/button";
