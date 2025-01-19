096c69a7c99344525c90419b8b71f80a4b004af7
Omar Zunic <contact@omarzunic.com>
2025-01-19 20:59:37 +0100

    Cloudflare ipcountry header

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,16 +1,44 @@
 import { isAuthenticated } from "@/lib/auth";
 import { getRequestConfig } from "next-intl/server";
+import { headers } from "next/headers";
 
-export const VALID_LOCALES = ["bs", "en"];
+export const VALID_LOCALES = ["bs"];
 
 export default getRequestConfig(async () => {
-	const user = await isAuthenticated();
-	const locale = VALID_LOCALES.includes(user?.language ?? "")
-		? user?.language
-		: "bs";
+	const [user, headersList] = await Promise.all([isAuthenticated(), headers()]);
 
+	// We get this from Cloudflare
+	const country = headersList.get("cf-ipcountry")?.toLowerCase() ?? "bs";
+
+	// If the user is logged in, they have a locale
+	if (user?.language) {
+		if (VALID_LOCALES.includes(user.language)) {
+			return {
+				locale: user.language,
+				messages: (await import(`../messages/${user.language}.json`)).default,
+			};
+		}
+
+		// If the user has an invalid locale, check if the request has a valid locale
+		if (VALID_LOCALES.includes(country)) {
+			return {
+				locale: country,
+				messages: (await import(`../messages/${country}.json`)).default,
+			};
+		}
+	}
+
+	// Otherwise, check if the request has a valid locale
+	if (VALID_LOCALES.includes(country)) {
+		return {
+			locale: country,
+			messages: (await import(`../messages/${country}.json`)).default,
+		};
+	}
+
+	// Default to bs
 	return {
-		locale: locale ?? "bs",
-		messages: (await import(`../messages/${locale}.json`)).default,
+		locale: "bs",
+		messages: (await import("../messages/bs.json")).default,
 	};
 });
