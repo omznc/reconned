ab5c490b45e6c602c20476e75c6a8e9ada2a1682
Omar Zunic <contact@omarzunic.com>
2025-01-22 13:12:53 +0100

    fix: build fix

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,7 @@
 // global.d.ts
 
 import type { formats } from "@/i18n/request";
-import type ba from "./messages/ba.json";
+import type ba from "./messages/bs.json";
 
 declare module "next-intl" {
 	interface AppConfig {
