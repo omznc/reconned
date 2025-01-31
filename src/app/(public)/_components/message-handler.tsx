0ec0e93aa23ab23bd4dacc74507d917e73ea6578
Omar Zunic <contact@omarzunic.com>
2025-01-31 11:16:29 +0100

    feat: add MessageHandler component for toast message management and update invitation handling
    Signed-off-by: Omar Zunic <contact@omarzunic.com>

---
+++ src/app/(public)/_components/message-handler.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useQueryState } from "nuqs";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
+
+export function MessageHandler() {
+    const [message] = useQueryState("message");
+    useEffect(() => {
+        toast.dismiss("message");
+        if (message) {
+            toast.success(decodeURIComponent(message), {
+                id: "message",
+            });
+        }
+    }, [message]);
+    return null;
+}
