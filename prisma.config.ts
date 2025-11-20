0ad96742455d3738e6d574a1949283e79b0d1e35
Omar Zunic <contact@omarzunic.com>
2025-11-20 16:41:49 +0100

    fix: attempt #1

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig, env } from "prisma/config";
-import "dotenv/config";
+import { config } from "dotenv";
+import { defineConfig } from "prisma/config";
+
+config();
 
 export default defineConfig({
 	schema: "prisma",
 	datasource: {
-		url: env("DATABASE_URL"),
+		url: process.env.DATABASE_URL as string,
 	},
 });
